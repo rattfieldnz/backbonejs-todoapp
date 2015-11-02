89ab653ff288ec317399541d7062fac9af5c3ed6
Rob Attfield <emailme@robertattfield.com>
2015-11-02 14:58:21 +1300

    Added code and functionality for todo model and colelction.

--- basic/assets/js/collections/todos.js
+++ basic/assets/js/collections/todos.js
@@ -1,3 +1,42 @@
 /**
  * Created by robattfield on 02-Nov-2015.
  */
+var app = app || {};
+
+/**
+ * The collection of Todos, stored in local storage with Backbone's *localStorage*.
+ */
+var TodoList = Backbone.Collection.extend({
+    // Reference model which todos are created with.
+    model: app.Todo,
+
+    // Save the collection in local storage, within the 'todos-backbone' namespace.
+    localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+    // Filter todo items that are finished.
+    completed: function(){
+        return this.filter(function(todo){
+            return todo.get('completed');
+        });
+    },
+
+    // Filter todo items that are not finished.
+    remaining: function(){
+        return this.without.apply(this, this.completed());
+    },
+    // Keep todos in sequential order, despite being stored by unordered GUID
+    // in database. Returned output is next order number.
+    nextOrder: function(){
+        if(!this.length){
+            return 1;
+        }
+        return this.last().get('order') + 1;
+    },
+    
+    // Sort todos by original insertion order.
+    comparator: function(todo){
+        return todo.get('order');
+    }
+});
+
+app.Todos = new TodoList();
--- basic/assets/js/models/todo.js
+++ basic/assets/js/models/todo.js
@@ -1,3 +1,18 @@
 /**
  * Created by robattfield on 02-Nov-2015.
  */
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+    // Default attributes for a to-do item
+    defaults: {
+        title: '',
+        completed: false
+    },
+    // Toggle the completion status of the to-do item.
+    toggle: function(){
+        this.save({
+            completed: !this.get('completed')
+        });
+    }
+});
